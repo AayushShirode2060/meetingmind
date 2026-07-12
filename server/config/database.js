8033a469b31bf2a165396d6f356364155baa6170
AayushShirode2060 <ayushshirode2060@gmail.com>
2026-07-12 15:33:54 +0530

    changes in db connection

--- server/config/database.js
+++ server/config/database.js
@@ -2,14 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    console.log("URI:", process.env.MONGODB_URI);
+   
 
     const conn = await mongoose.connect(process.env.MONGODB_URI);
 
     console.log("✅ Connected!");
-    console.log(conn.connection.readyState);
-    console.log(conn.connection.host);
-    console.log(conn.connection.name);
+    
   } catch (err) {
     console.error("FULL ERROR:");
     console.error(err);
